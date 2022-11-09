ac1f4570a606b8c2eacadecb6c72c26926e95f2a
Sreejit <sreesen2003@gmail.com>
2022-11-09 13:05:13 +0530

    Modified files

--- practice.js
+++ practice.js
@@ -1,3 +1,12 @@
 for(let i = 0; i<10; i++){
     console.log(i);
 }
+
+let messages = [
+    "Hey",
+    "Hello",
+    "Hi"
+]
+for(let i = 0; i<messages.length; i++){
+    console.log(messages[i]);
+}
